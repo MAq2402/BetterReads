57352686c6df6f6971293a739f68c9887911a89a
MAq2402 <maq0297@gmail.com>
2024-12-11 11:08:10 +0100

    Api gateway configuration

--- BetterReads.ApiGateway/BetterReads.ApiGateway.Web/Program.cs
+++ BetterReads.ApiGateway/BetterReads.ApiGateway.Web/Program.cs
@@ -1,9 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,9 +17,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+await app.UseOcelot();
 app.UseHttpsRedirection();
-
-app.MapGet("/hello-world", () => "Hello world From Api Gateway Service")
-    .WithName("HelloWorld");
 
 app.Run();
